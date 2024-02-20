fba836eb92bf75aa352b835f5a1c2a37a9b0aa77
mxstake <matt.liegeois@gmail.com>
2024-02-20 16:17:14 +0100

    [add] a ball.

--- main.cpp
+++ main.cpp
@@ -3,20 +3,60 @@
 
 using namespace std;
 
+int windowWidht, windowHeight;
+
+float ballX, ballY, ballSpeedX,ballSpeedY;
+
 int main() {
 
-    
+    windowWidht = 500;
+    windowHeight = 750;
     cout << "Hello World" << endl;
 
-    InitWindow(300, 300, "My first Raylib window!");
+    void Ball(void);
+
+    InitWindow(windowHeight, windowWidht, "Dat window");
     SetTargetFPS(60);
+    
+    ballX = windowHeight/2;
+    ballY = windowWidht / 2;
+    ballSpeedX = 1;
+    ballSpeedY = 1;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(DARKGREEN);
+        ClearBackground(SKYBLUE);
+        Ball();
+        DrawCircle(ballX, ballY, 8, WHITE);
+       
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
+}
+
+void Ball()
+{
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballX < 0)
+    {
+        ballSpeedX = -ballSpeedX;
+        
+    }
+    else if (ballX > 750)
+    {
+        ballSpeedX = -ballSpeedX;
+    }
+
+    if (ballY < 0)
+    {
+        ballSpeedY = -ballSpeedY;
+    }
+    else if (ballY > 500)
+    {
+        ballSpeedY = -ballSpeedY;
+    }
 }
